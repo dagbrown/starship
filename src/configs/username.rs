46989d0169c1cf2d662702f900e4402e91f75423
Dave Brown <dagbrown@lart.ca>
2021-07-15 14:36:52 +0900

    feat: refactor modules to use format strings (#1374)

--- src/configs/username.rs
+++ src/configs/username.rs
@@ -1,14 +1,12 @@
 use crate::config::{ModuleConfig, RootModuleConfig};
 
-use ansi_term::{Color, Style};
 use starship_module_config_derive::ModuleConfig;
 
 #[derive(Clone, ModuleConfig)]
 pub struct UsernameConfig<'a> {
-    pub prefix: &'a str,
-    pub suffix: &'a str,
-    pub style_root: Style,
-    pub style_user: Style,
+    pub format: &'a str,
+    pub style_root: &'a str,
+    pub style_user: &'a str,
     pub show_always: bool,
     pub disabled: bool,
 }
@@ -16,10 +14,9 @@ pub struct UsernameConfig<'a> {
 impl<'a> RootModuleConfig<'a> for UsernameConfig<'a> {
     fn new() -> Self {
         UsernameConfig {
-            style_root: Color::Red.bold(),
-            style_user: Color::Yellow.bold(),
-            prefix: "",
-            suffix: "",
+            format: "via [$user]($style) ",
+            style_root: "red bold",
+            style_user: "yellow bold",
             show_always: false,
             disabled: false,
         }
